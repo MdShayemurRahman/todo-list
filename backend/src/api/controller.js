1619cfe93505eeda173c18f729240eb0a718e452
Md Shayemur Rahman <sayemurrahman99@gmail.com>
2021-12-25 23:40:06 +0200

    implementation of router functions

--- backend/src/api/controller.js
+++ backend/src/api/controller.js
@@ -1,19 +1,54 @@
-export const getAll = (req, res) => {
-  res.send("getAllById");
+import Todo from "../Database/model.js";
+
+export const getAll = async (req, res) => {
+  try {
+    let todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const getById = (req, res) => {
-  res.send("getById");
+export const getById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const todos = await Todo.findById(id);
+    res.json(todos);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const postData = (req, res) => {
-  res.send("postData");
+export const postData = async (req, res) => {
+  const addData = req.body;
+  const newData = new Todo(addData);
+
+  try {
+    await newData.save();
+    res.send("successfully stored data");
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const updateData = (req, res) => {
-  res.send("updateData");
+export const updateData = async (req, res) => {
+  const _id = req.params.id;
+  const data = req.body;
+  const updatedData = new Todo(data);
+
+  try {
+    const update = await Todo.updateOne({ _id }, updatedData);
+    res.json(update);
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const deleteData = (req, res) => {
-  res.send("deleteData");
+export const deleteData = async (req, res) => {
+  try {
+    await Todo.deleteOne({ _id: req.params.id });
+    res.json("data deleted successfully!");
+  } catch (error) {
+    console.error(error);
+  }
 };
